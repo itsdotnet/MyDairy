c6435bb0e1b41180bb5ebf5c0efb4d908c1e0d21
iskandar kodirov <eiscocyber@gmail.com>
2023-08-26 20:17:25 +0500

    IRepository(Generic) and IUnitOfWork Writed for MyDairy.DAL

---
+++ src/MyDairy.DAL/Constexts/MyDairyDbContext.cs
@@ -0,0 +1,16 @@
+﻿using MyDairy.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace MyDairy.DAL.Constexts;
+
+public class MyDairyDbContext : DbContext
+{
+    public MyDairyDbContext(DbContextOptions<MyDairyDbContext> options) : base(options)
+    { }
+
+    DbSet<User> Users { get; set; }
+
+    DbSet<Note> Notes { get; set; }
+
+    DbSet<Attachment> Attachments { get; set; }
+}
---
+++ src/MyDairy.DAL/IRepositories/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿using MyDairy.Domain.Entities;
+
+namespace MyDairy.DAL.IRepositories;
+
+public interface IUnitOfWork : IDisposable
+{
+    IRepository<User> UserRepository { get; }
+    IRepository<Note> NotesRepository { get; }
+    IRepository<Attachment> AttachmentRepository { get; }
+    Task<bool> SaveAsync();
+}
