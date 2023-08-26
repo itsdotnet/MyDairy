ad2202c1392b9216eb6237bcfae96fd0c38efd39
iskandar kodirov <eiscocyber@gmail.com>
2023-08-26 20:35:50 +0500

    Helpers added

---
+++ src/MyDairy.Service/Helpers/PasswordHasher.cs
@@ -0,0 +1,16 @@
+﻿namespace MyDairy.Service.Helpers;
+
+public static class PasswordHasher
+{
+    public static string Hash(this string password)
+    {
+        string salt = Guid.NewGuid().ToString();
+        string hash = BCrypt.Net.BCrypt.HashPassword(password + salt);
+        return hash;
+    }
+
+    public static bool Verify(this string password, string hash)
+    {
+        return BCrypt.Net.BCrypt.Verify(password, hash);
+    }
+}
---
+++ src/MyDairy.Service/Helpers/PathHelper.cs
@@ -0,0 +1,6 @@
+﻿namespace MyDairy.Service.Helpers;
+
+public static class PathHelper
+{
+    public static string WebRootPath { get; set; }
+}
