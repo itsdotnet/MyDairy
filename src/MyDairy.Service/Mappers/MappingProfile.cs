11f44cb35faa3b10fac79447531d567a5e00beb4
iskandar kodirov <eiscocyber@gmail.com>
2023-08-26 20:36:55 +0500

    Mapping Profile added

---
+++ src/MyDairy.Service/Mappers/MappingProfile.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using MyDairy.Domain.Entities;
+using MyDairy.Service.DTOs.Attachment;
+using MyDairy.Service.DTOs.Notes;
+using MyDairy.Service.DTOs.Users;
+
+namespace MyDairy.Service.Mappers;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        //user
+
+        CreateMap<UserCreationDto, User>().ReverseMap();
+        CreateMap<UserUpdateDto, User>().ReverseMap();
+        CreateMap<User, UserResultDto>().ReverseMap();
+
+        //note
+
+        CreateMap<NoteCreationDto, Note>().ReverseMap();
+        CreateMap<NoteUpdateDto, Note>().ReverseMap();
+        CreateMap<Note, NoteResultDto>().ReverseMap();
+
+        //attachment
+
+        CreateMap<Attachment, AttachmentResultDto>().ReverseMap();
+    }
+}
