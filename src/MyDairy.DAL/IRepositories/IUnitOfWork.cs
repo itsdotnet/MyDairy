1161c7775de896b63bafd3b0c6f15ca2334ae7c6
iskandar kodirov <eiscocyber@gmail.com>
2023-08-26 20:21:06 +0500

    Repository and unitofwork added

--- src/MyDairy.DAL/IRepositories/IUnitOfWork.cs
+++ src/MyDairy.DAL/IRepositories/IUnitOfWork.cs
@@ -5,7 +5,7 @@ namespace MyDairy.DAL.IRepositories;
 public interface IUnitOfWork : IDisposable
 {
     IRepository<User> UserRepository { get; }
-    IRepository<Note> NotesRepository { get; }
+    IRepository<Note> NoteRepository { get; }
     IRepository<Attachment> AttachmentRepository { get; }
     Task<bool> SaveAsync();
 }
---
+++ src/MyDairy.DAL/Repositories/UnitOfWork.cs
@@ -0,0 +1,36 @@
+﻿using MyDairy.DAL.Constexts;
+using MyDairy.DAL.IRepositories;
+using MyDairy.DAL.Repositories;
+using MyDairy.Domain.Entities;
+
+namespace MyDairy.DAL.Repository;
+
+public class UnitOfWork : IUnitOfWork
+{
+
+    private readonly MyDairyDbContext dbContext;
+
+    public UnitOfWork(MyDairyDbContext dbContext)
+    {
+        this.dbContext = dbContext;
+        UserRepository = new Repository<User>(dbContext);
+        NoteRepository = new Repository<Note>(dbContext);
+        AttachmentRepository = new Repository<Attachment>(dbContext);
+    }
+
+    public IRepository<User> UserRepository { get; }
+
+    public IRepository<Note> NoteRepository { get; }
+
+    public IRepository<Attachment> AttachmentRepository { get; }
+
+    public void Dispose()
+    {
+        GC.SuppressFinalize(true);
+    }
+
+    public async Task<bool> SaveAsync()
+    {
+        return await dbContext.SaveChangesAsync() > 0;
+    }
+}
